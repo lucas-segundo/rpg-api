7c9f7fb2f483fe03bae91adb2b440e08fb9791f0
Lucas Fernandes <lucas.fernanades.app@gmail.com>
2023-06-28 08:07:34 -0300

    feat: adds classes table

---
+++ src/infra/prisma/migrations/20230628102141_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "classes" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "classes_pkey" PRIMARY KEY ("id")
+);
