66da7928e06167426114ff9f4fb8e744def7b2b9
Lucas Fernandes <lucas.fernanades.app@gmail.com>
2023-06-28 17:58:25 -0300

    test: should return class created

--- src/infra/prisma/repositories/ClassCreater/test.ts
+++ src/infra/prisma/repositories/ClassCreater/test.ts
@@ -1,3 +1,4 @@
+import { ClassCreaterRepoResult } from 'app/interfaces/ClassCreaterRepo'
 import { mockClassCreaterParams } from 'domain/useCases/ClassCreater/mock'
 import { prismaMock } from 'infra/prisma/mock'
 import { mockDbClass } from 'infra/prisma/mocks/class'
@@ -17,5 +18,21 @@ describe('PrismaClassCreaterRepo', () => {
         title: params.title,
       },
     })
+  })
+
+  it('should return class created', async () => {
+    const sut = new PrismaClassCreaterRepo()
+
+    const dbClassCreated = mockDbClass()
+    prismaMock.class.create.mockResolvedValue(dbClassCreated)
+
+    const params = mockClassCreaterParams()
+    const result = await sut.create(params)
+
+    const expectedResult: ClassCreaterRepoResult = {
+      id: dbClassCreated.id.toString(),
+      title: params.title,
+    }
+    expect(result).toEqual(expectedResult)
   })
 })
