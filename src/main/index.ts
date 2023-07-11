1cb7dfd6ee168d75d56a310696a63be133a5b7b1
Lucas Fernandes <lucas.fernanades.app@gmail.com>
2023-07-11 17:32:22 -0300

    chore: adds swagger

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './routes/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('RPG API')
+    .setDescription('The CRUD for the API')
+    .setVersion('1.0')
+    .addTag('rpg')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
